9b83c2e023eeeb6538c415bbf5d2756da9954502
banana-king778 <74279150+banana-king778@users.noreply.github.com>
2020-11-11 08:24:50 -0600

    Update TimeBot.py

--- TimeBot.py
+++ TimeBot.py
@@ -22,9 +22,9 @@ async def on_message(message):
         return
     elif (':') in message.content:
         text = message.content
-        hour = text[text.find(":") - 1:].split(':')[0]
+        hour = text[text.find(":") - 2:].split(':')[0]
         minutes = text[text.find(":") + 1:].split()[0]
-        if 'am' in message.content:
+        if ' am' in message.content:
             ampm = text[text.find(":") + 3:].split()[0]
             ampm = ampm.replace('?', '')
             if ampm == 'am':
@@ -57,6 +57,21 @@ async def on_message(message):
                     embed.add_field(name=f'{year.year}:{timedif.days + dateadd}:{int(hours)}:{minutes}:00 A.C (UTC +0)',
                                     value=f'Original Message: {message.content}')
                     await message.channel.send(embed=embed)
+        elif ' pm' in message.content:
+            hourr = 12
+            hours = int(hour) + hourr + 6
+            if hours >= 24:
+                hours = hours - 24
+                dateadd = 2
+                embed = discord.Embed(colour=discord.Colour.dark_blue())
+                embed.add_field(name=f'{year.year}:{timedif.days + dateadd}:{int(hours)}:{minutes}:00 A.C (UTC +0)',
+                                value=f'Original Message: {message.content}')
+                await message.channel.send(embed=embed)
+            else:
+                embed = discord.Embed(colour=discord.Colour.dark_blue())
+                embed.add_field(name=f'{year.year}:{timedif.days + dateadd}:{int(hours)}:{minutes}:00 A.C (UTC +0)',
+                                value=f'Original Message: {message.content}')
+                await message.channel.send(embed=embed)
         else:
             hours = int(hour) + hourr + 6
             if hours >= 24:
