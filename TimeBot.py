288173703fa4865bdd4d22fff7db470867d3c557
banana-king778 <gamingthingemail@gmail.com>
2022-11-21 22:10:43 -0600

    Update TimeBot.py

--- TimeBot.py
+++ TimeBot.py
@@ -9,12 +9,15 @@ from discord import ui
 import random
 from threading import Timer
 import requests
-Time = time
 TOKEN = 'Nzc1ODM2NTgyNjYxMDYyNzM3.GN8Wkh.L2KE7y0_yRlJxlxzxXAxk_iWUGVuO20k3ECP-o'
 guild = discord.Object(id="713404775357743216")
 error_responses_file = open("error_responses.txt", "r")
 error_responses = (error_responses_file.read()).split("\n")
 error_responses_file.close()
+
+def get_current_time():
+    current_time = datetime.now().strftime("%H:%M")
+    return current_time
 
 class aclient(discord.Client):
     def __init__(self):
@@ -34,7 +37,7 @@ class aclient(discord.Client):
     async def pushmonitor(self):
         await self.wait_until_ready()
         while not self.is_closed():
-            response = requests.get("https://sg.bananaserver.tk/api/push/eO0hZAL0OK?status=up&msg=OK&ping=")
+            response = requests.get("https://uptime.bananaproject.tk/api/push/rGEAHK9Jfp?status=up&msg=OK&ping=")
             await asyncio.sleep(30)
 
 client = aclient()
@@ -47,19 +50,41 @@ class TimeModal(ui.Modal, title="Unix Time Converter"):
     modalmonth = ui.TextInput(label="Month", style=discord.TextStyle.short, max_length=6, placeholder="Input Month", default=todaymonth)
     modalday = ui.TextInput(label="Day", style=discord.TextStyle.short, max_length= 6, placeholder= "Input Day", default=todayday)
     modaltime = ui.TextInput(label = 'Time', style=discord.TextStyle.short, max_length= 6, placeholder= "Input Time", default=todaytime)
-    async def on_submit(self, interaction: discord.Interaction,):
+    async def on_submit(self, interaction: discord.Interaction):
         utime = datetime.strptime(str((self.modaltime)), "%H:%M")
         inputtime = datetime(datetime.now().year, int(str(self.modalmonth)), int(str(self.modalday)), utime.hour, utime.minute)
-        preunixtime = Time.mktime(inputtime.timetuple())
+        preunixtime = time.mktime(inputtime.timetuple())
         unixtime = f"<t:{int(preunixtime)}"
-        embed = discord.Embed(title= self.title, description = f"{unixtime}>\n{unixtime} >", timestamp = datetime.now(), color = discord.Colour.yellow())
+        embed = discord.Embed(title= self.title, description = f"{unixtime}>\n{unixtime}\n *Add a '>' to the end*", timestamp = datetime.now(), color = discord.Colour.yellow())
         embed.set_author(name=interaction.user, icon_url=interaction.user.avatar)
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
         await interaction.response.send_message(f"{random.choice(error_responses)} **Error:** {error}")
 
-@tree.command(name = "utime", description = "testing unix time thingy", guild=guild)
+class KianForm(ui.Modal, title="Kian Clock-in/out Form"):
+    civ_callsign = ui.TextInput(label="CC Callsign", style=discord.TextStyle.short, max_length= 7, placeholder= "Input CC Callsign", default='CIV-100')
+    rank = ui.TextInput(label = 'Rank', style=discord.TextStyle.short, max_length= 15, placeholder= "Input Rank", default='Supervisor')
+    clock_in = ui.TextInput(label='Clock In', style=discord.TextStyle.short, max_length=6, placeholder='Input Clock In')
+    clock_out = ui.TextInput(label='Clock Out', style=discord.TextStyle.short, max_length=6, placeholder='Input Clock Out')
+    other = ui.TextInput(label = 'Other (Put Recovery or Tranport)', style=discord.TextStyle.short, max_length=9, placeholder='Input Recovery or Transport', default='Recovery')
+    async def on_submit(self, interaction: discord.Interaction):
+        discord_user = interaction.user
+        rate_experience = '6'
+        business = 'Kian Services'
+        clock_in = datetime.strptime(str((self.clock_in)), "%H:%M")
+        clock_out = datetime.strptime(str((self.clock_out)), "%H:%M")
+        total_time = clock_out - clock_in
+        await interaction.response.send_message(f'User: {discord_user}\nCiv Callsign: {self.civ_callsign}\nBusiness: {business}\nRank: {self.rank}\nClock In: {self.clock_in}\nClock Out: {self.clock_out}\nTotal Time: {total_time}\nRating: {rate_experience}\nOther: {self.other}')
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message(f"{random.choice(error_responses)} **Error:** {error}")
+
+
+@tree.command(name = "unixtime", description = "Unix Time Converter", guild=guild)
 async def self(interaction: discord.Interaction):
     await interaction.response.send_modal(TimeModal())
+
+@tree.command(name = "kianform", description = "Kian Clock-in/out Form", guild=guild)
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_modal(KianForm())
 
 client.run(TOKEN)
