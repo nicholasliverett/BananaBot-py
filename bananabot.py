ca9c9dbec3312e69c937d61d534c557bf69c8d47
banana-king778 <74279150+banana-king778@users.noreply.github.com>
2023-02-26 00:15:12 -0600

    Update bananabot.py

--- bananabot.py
+++ bananabot.py
@@ -31,9 +31,9 @@ class aclient(discord.Client):
         self.tree.copy_global_to(guild=guild)
         await self.tree.sync(guild=guild)
 
-tree = app_commands.CommandTree()
 intents = discord.Intents.default()        
 client = aclient(intents=intents)
+tree = app_commands.CommandTree(client)
 
 
 class TimeModal(ui.Modal, title="Unix Time Converter"):
